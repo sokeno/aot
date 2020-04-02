7d6072a4b817ec0246e975ca400b6b3caa0d4586
sokeno <steveokeno@gmail.com>
2020-04-02 19:00:01 +0300

    component generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+
+
+const appRoutes: Routes = [
+  {path:'sign-in' , component : SignInComponent},
+  {path:'sign-up' , component : SignUpComponent},
+  {path:'',component: IndexComponent , pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignUpComponent,
+    SignInComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
