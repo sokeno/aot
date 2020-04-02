57742b89485e25356295fb2c33398f1396d6185f
sokeno <steveokeno@gmail.com>
2020-04-02 22:22:19 +0300

    updates

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IGroup } from '../shared/Igroup';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-group',
@@ -10,38 +11,58 @@ export class GroupComponent implements OnInit {
 
   pageTitle: string = "Groups";
 
-  name:string = "";
+  name: string = "";
 
-  errorMessage : string = "";
+  errorMessage: string = "";
 
   switchForm: boolean = false;
 
   groups: IGroup[] = [
     {
-      "id":3,
+      "id": 3,
       "name": 'developers',
       "memberCount": 40
     },
     {
-      "id":4,
+      "id": 4,
       "name": "Marketing",
       "memberCount": 20
     }
   ];
-  constructor() { }
+  constructor(private router: Router, ) { }
 
   ngOnInit(): void {
   }
 
   validateField(): boolean {
-    return this.name.trim()=="" ? false : true;
+
+    if (this.groups.some(group => group.name == this.name)) return false;
+
+    return this.name.trim() == "" ? false : true;
   }
 
-  newGroup() : void{
+  newGroup(): void {
     if (this.validateField()) {
-      
-    }else{
-      this.errorMessage = "Enter a new name";
+
+      let obj: IGroup = {
+        "id": Date.now(),
+        "name": this.name,
+        "memberCount": 0
+      };
+
+      this.name = "";
+
+      this.errorMessage = "Group added successfully";
+
+      setTimeout(()=>{
+        this.errorMessage = "";
+      },1000);
+
+      this.groups.unshift(obj);
+
+
+    } else {
+      this.errorMessage = "Enter unique name";
     }
     console.log(this.name);
   }
