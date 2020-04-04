d8cd41722196020672a076e899b98d26383c977f
sokeno <steveokeno@gmail.com>
2020-04-04 12:11:29 +0300

    user should be able to create account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { IndexComponent } from './index/index.component';
+import { GroupComponent } from './group/group.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sign-in' , component : SignInComponent},
+  {path:'sign-up' , component : SignUpComponent},
+  {path : 'groups' , component :GroupComponent},
+  {path:'users' , component : UserComponent},
+  {path:'',component: SignUpComponent , pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { GroupComponent } from './group/group.component';
 import { UserComponent } from './user/user.component';
-
-
-const appRoutes: Routes = [
-  {path:'sign-in' , component : SignInComponent},
-  {path:'sign-up' , component : SignUpComponent},
-  {path : 'groups' , component :GroupComponent},
-  {path:'users' , component : UserComponent},
-  {path:'',component: IndexComponent , pathMatch: 'full'}
-]
 
 @NgModule({
   declarations: [
@@ -34,11 +24,10 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
     AppRoutingModule,
     FormsModule
   ],
   providers: [],
-  bootstrap: [SignUpComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
